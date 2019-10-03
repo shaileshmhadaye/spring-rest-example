25370482f236e3b59435c23e4701f68047593a5d
shaileshmhadaye <shaileshmhadaye2@gmail.com>
2019-10-03 13:59:03 +0530

    Displayed API data using webflux.

--- src/main/java/guru/springframework/config/RestTemplateConfig.java
+++ src/main/java/guru/springframework/config/RestTemplateConfig.java
@@ -4,8 +4,6 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
-import org.thymeleaf.templatemode.TemplateMode;
-import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
 @Configuration
 public class RestTemplateConfig {
